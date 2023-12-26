24d4611a7a4c80a1209db78c5f8c2cb4a24e7d7e
Fenrisulfr66 <gabrielgplxz@gmail.com>
2023-12-26 20:58:50 -0300

    rename

--- .config/rofi/powermenu/powermenu.sh
+++ .config/rofi/powermenu/powermenu.sh
@@ -1,41 +1,108 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ## Author : Aditya Shakya (adi1090x)
-## Mail : adi1090x@gmail.com
 ## Github : @adi1090x
-## Reddit : @adi1090x
+#
+## Rofi   : Power Menu
+#
+## Available Styles
+#
+## style-1   style-2   style-3   style-4   style-5
 
-# Available Styles
-# >> Styles Below Only Works With "rofi-git(AUR)", Current Version: 1.5.4-76-gca067234
-# full     full_circle     full_rounded     full_alt
-# card     card_circle     column     column_circle
-# row     row_alt     row_circle
-# single     single_circle     single_full     single_full_circle     single_rounded     single_text
+# Current Theme
+dir="~/.config/rofi/powermenu"
+theme="dark"
 
-style="power"
-rofi_command="rofi -theme ~/.config/rofi/powermenu/dark.rasi"
-#dir="~/.config/rofi/powermenu"
-#theme="dark"
-uptime=$(uptime -p)
-my_hostname=$(hostname)
+# CMDs
+#uptime="`uptime -p | sed -e 's/up //g'`"
+host=`hostname`
 
 # Options
-shutdown=" Shutdown"
-reboot=" Reboot"
-logout=" Logout"
+shutdown=' Shutdown'
+reboot=' Reboot'
+logout=' Logout'
+yes=' Yes'
+no=' No'
 
-# Variable passed to rofi
-options="$shutdown\n$reboot\n$logout"
+# Rofi CMD
+rofi_cmd() {
+	rofi -dmenu \
+		-p "$host" \
+		-theme ${dir}/${theme}.rasi
+}
 
-chosen="$(echo -e "$options" | $rofi_command -p " Power-Menu " -dmenu -selected-row 2)"
-case $chosen in
+# Confirmation CMD
+confirm_cmd() {
+	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
+		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
+		-theme-str 'listview {columns: 2; lines: 1;}' \
+		-theme-str 'element-text {horizontal-align: 0.5;}' \
+		-theme-str 'textbox {horizontal-align: 0.5;}' \
+		-dmenu \
+		-p 'Confirmation' \
+		-mesg 'Are you Sure?' \
+		-theme ${dir}/${theme}.rasi
+}
+
+# Ask for confirmation
+confirm_exit() {
+	echo -e "$yes\n$no" | confirm_cmd
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+	echo -e "$shutdown\n$reboot\n$logout" | rofi_cmd
+}
+
+# Execute Command
+run_cmd() {
+	selected="$(confirm_exit)"
+	if [[ "$selected" == "$yes" ]]; then
+		if [[ $1 == '--shutdown' ]]; then
+			systemctl poweroff
+		elif [[ $1 == '--reboot' ]]; then
+			systemctl reboot
+		elif [[ $1 == '--suspend' ]]; then
+			mpc -q pause
+			amixer set Master mute
+			systemctl suspend
+		elif [[ $1 == '--logout' ]]; then
+			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
+				openbox --exit
+			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
+				bspc quit
+			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
+				i3-msg exit
+			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
+				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+			fi
+		fi
+	else
+		exit 0
+	fi
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
     $shutdown)
-        sudo shutdown -h now
-	;;
+		run_cmd --shutdown
+        ;;
     $reboot)
-        sudo shutdown -r now
+		run_cmd --reboot
+        ;;
+    $lock)
+		if [[ -x '/usr/bin/betterlockscreen' ]]; then
+			betterlockscreen -l
+		elif [[ -x '/usr/bin/i3lock' ]]; then
+			i3lock
+		fi
+        ;;
+    $suspend)
+		run_cmd --suspend
         ;;
     $logout)
-        pkill -KILL -u $USER
+		run_cmd --logout
         ;;
 esac
+
